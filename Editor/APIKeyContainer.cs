fae2a5537efd29ab67626e23718b1ec2ef2887f2
Trikeri_Omni <troyvmaynard@gmail.com>
2023-04-25 16:10:10 -0400

    Initial scripts for the package

---
+++ Editor/APIKeyContainer.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class APIKeyContainer
+{
+    public static string APIKey = "nexus_pk_7784c3faa6e94c1fad8aceab27ab31e6";
+}
