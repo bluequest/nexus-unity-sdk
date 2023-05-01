7e3b40271dce1dd80bb39591d5d680acdf18e7a1
Voltaire Stephen Medina <vmedina@punyhuman.com>
2023-05-01 17:16:52 -0400

    Add functional PingAttributions runtime test

--- SDKTests/Runtime/NexusUnitySDKRuntimeTests.cs
+++ SDKTests/Runtime/NexusUnitySDKRuntimeTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -6,11 +7,30 @@ using UnityEngine.TestTools;
 
 public class NexusUnitySDKRuntimeTests
 {
-    [UnityTest]
-    public IEnumerator GameFlowTest()
-    {
-		// Use the Assert class to test conditions
-		Assert.AreEqual(1, 1);
-		yield return null;
-    }
+	[UnityTest]
+	public IEnumerator NexusUnitySDK_PingAttributionsTest()
+	{
+		yield return new MonoBehaviourTest<PingAttributionsTest>();
+	}
+
+	public class PingAttributionsTest : MonoBehaviour, IMonoBehaviourTest
+	{
+		private int frameCount;
+		private bool bPingAttributionsResponse = false;
+		public bool IsTestFinished
+		{
+			get { return bPingAttributionsResponse; }
+		}
+
+		void Start()
+		{
+			UnityEngine.Debug.Log("Beginning Coroutine to request PingAttributions!");
+			StartCoroutine(NexusAPI_Attributions.PingAttributionsRequest((isSuccess) =>
+			{
+				UnityEngine.Debug.Log("Received response of value: " + isSuccess);
+				Assert.AreEqual(isSuccess, true);
+				bPingAttributionsResponse = true;
+			}));
+		}
+	}
 }
